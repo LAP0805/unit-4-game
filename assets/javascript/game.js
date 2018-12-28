5de731346b17abf07e8245ecac6add602751e288
Janxxy <lesley.pond@gmail.com>
2018-12-28 00:04:04 -0500

    basic functioning game

---
+++ assets/javascript/game.js
@@ -0,0 +1,99 @@
+var yourScore = 0;
+var randomNumbo = 0;
+var wins= 0;
+var losses = 0;
+
+
+$("#your-number").text(yourScore);
+$("#random-number").text(randomNumbo);
+$("#wins").text(wins);
+$("#losses").text(losses);
+
+//function to generate random target number//
+function randomNumber(){
+    var min=50; 
+    var max=100;  
+    var random =Math.floor(Math.random() * (+max - +min)) + +min; 
+    console.log(random);
+    randomNumbo = random;
+    $("#random-number").text(randomNumbo);
+   
+}
+//function to set random number for each crystal//
+function randomCrystal(){
+var crystalArray = [];
+var cr1 = $("#crystal1");
+var cr2 = $("#crystal2");
+var cr3 = $("#crystal3");
+var cr4 = $("#crystal4");
+crystalArray.push(cr1,cr2,cr3,cr4);
+crystalArray.forEach(function(crystal){
+    var min=1; 
+    var max=15;  
+    var random =Math.floor(Math.random() * (+max - +min)) + +min; 
+   crystal.attr("data-number", random);
+});
+
+console.log(crystalArray);
+};
+
+//function to add data number to score when crystal clicked
+
+$("#crystal1").on("click", function(){
+    var go = $("#crystal1").data("number");
+ yourScore = yourScore + go;
+ $("#your-number").text(yourScore);
+ winLose()
+});
+$("#crystal2").on("click", function(){
+    var go = $("#crystal2").data("number");
+ yourScore = yourScore + go;
+ $("#your-number").text(yourScore);
+ winLose()
+});
+$("#crystal3").on("click", function(){
+    var go = $("#crystal3").data("number");
+ yourScore = yourScore + go;
+ $("#your-number").text(yourScore);
+ winLose()
+});
+$("#crystal4").on("click", function(){
+    var go = $("#crystal4").data("number");
+ yourScore = yourScore + go;
+ $("#your-number").text(yourScore);
+ winLose()
+});
+
+
+//function for win or lose
+function winLose() {
+var you = yourScore;
+var me = randomNumbo;
+if (you == me){
+ wins++;
+ $("#wins").text(wins);
+ resetGame()
+
+}
+else if(you > me){
+    losses ++;
+    $("#losses").text(losses);
+    resetGame()
+}
+}
+
+//function to reset game 
+function resetGame(){
+ yourScore = 0;
+ $("#your-number").text(yourScore);
+ randomNumber();
+ randomCrystal();
+
+
+}
+
+randomNumber()
+randomCrystal()
+
+
+
