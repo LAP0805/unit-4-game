9ce2dfe8796708e83bd9b9fb7d0d8716e5d13c83
Janxxy <lesley.pond@gmail.com>
2018-12-29 01:45:39 -0500

    fixed stupidity

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,46 +14,44 @@ $("#random-number").text(randomNumbo);
 $("#wins").text(wins);
 $("#losses").text(losses);
 
-//function to push unique random numbers into an array (found code and modified-I'm not this smart)
-let anArrayOfUniqueNumbers = [];
-let numberGenerator = function(arr) {
-  if (arr.length >= 4) return;
-  var min=1; 
-    var max=12;  
-    var newNumber = Math.floor(Math.random() * (+max - +min)) + +min; 
-   
-  if (arr.indexOf(newNumber) < 0) {
-    arr.push(newNumber);
-  }
-  numberGenerator(arr);
-};
-numberGenerator(anArrayOfUniqueNumbers);
-console.log(anArrayOfUniqueNumbers);
-
 //function to generate random target number//
 function randomNumber(){
-    var min=19; 
-    var max=120;  
+    var min=50; 
+    var max=100;  
     var random =Math.floor(Math.random() * (+max - +min)) + +min; 
+    console.log(random);
     randomNumbo = random;
     $("#random-number").text(randomNumbo);
    
 }
-//function to set random number for each crystal and push to youScore when clicked.
+//function to set random number for each crystal and push to yourScore when clicked.
 function randomCrystal(){
-    
-crystalArray.forEach(function(crystal,i){
-        var random = anArrayOfUniqueNumbers[i]; 
-    console.log(random);
-      crystal.attr("data-number", random); 
-   crystal.on("click", function(){
-    var go = crystal.data("number");
-yourScore = yourScore + go;
-  $("#your-number").text(yourScore);
-  winLose()
+crystalArray.forEach(function(crystal){
+    var min=1; 
+    var max=15;  
+    var newR =Math.floor(Math.random() * (+max - +min)) + +min; 
+   crystal.attr("data-number", newR);
+   
 });
-});
+
 };
+
+
+  $(".crystal-class").on("click", function(){
+      var dataNumber = ($(this).attr("data-number")); 
+      var newDataNumber = parseInt(dataNumber);
+console.log(dataNumber);
+   yourScore = yourScore + newDataNumber;
+   $("#your-number").text(yourScore);
+   winLose();
+
+   });
+    
+
+
+
+
+
 
 //function for win or lose
 function winLose() {
@@ -81,9 +79,7 @@ function resetGame(){
 
 
 }
-//Call functions
+
 randomNumber()
 randomCrystal()
-
-
 
